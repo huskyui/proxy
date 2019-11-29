de695a6d5010b863d882336e812e22d96e59f506
huskyui <2207019991@qq.com>
2019-11-29 09:58:18 +0800

    update .ignore file

--- src/main/java/com/husky/proxy/Proxy.java
+++ src/main/java/com/husky/proxy/Proxy.java
@@ -1,10 +1,18 @@
 package com.husky.proxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
 /**
  * @author husky
  * @date 2018/12/12 18:50
  */
 public class Proxy implements Subject {
+    private static Logger logger = LoggerFactory.getLogger(Proxy.class);
+
     private Subject subject = null;
 
     public Proxy(Subject subject){
@@ -20,9 +28,16 @@ public class Proxy implements Subject {
 
     public void before(){
         //do something
+        logger.info("start :{}",LocalDateTime.now());
     }
 
     public void after(){
         //do something
+        logger.info("end :{}",LocalDateTime.now());
+    }
+
+    public static void main(String[] args) {
+        Proxy proxy = new Proxy(new RealSubject());
+        proxy.request();
     }
 }
--- src/main/java/com/husky/proxy/RealSubject.java
+++ src/main/java/com/husky/proxy/RealSubject.java
@@ -1,13 +1,18 @@
 package com.husky.proxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author husky
  * @date 2018/12/12 18:48
  */
 public class RealSubject implements Subject {
+    private static Logger logger = LoggerFactory.getLogger(RealSubject.class);
 
     @Override
     public void request() {
         //业务逻辑处理
+        logger.info("real message : {}","fuck you");
     }
 }
